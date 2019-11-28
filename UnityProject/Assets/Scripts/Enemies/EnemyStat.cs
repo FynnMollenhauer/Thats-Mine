4514f4cbfac5f49af3fcfc2470473df63cebc8c3
Tống Tùng Giang <tongtunggiang@gmail.com>
2019-11-28 12:34:22 +0000

    Enemy movement & enemy damage/player damage

--- UnityProject/Assets/Scripts/Enemies/EnemyStat.cs
+++ UnityProject/Assets/Scripts/Enemies/EnemyStat.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStat : MonoBehaviour
+public class EnemyStat : MonoBehaviour, IDamagable
 {
     [SerializeField] float health;
     [SerializeField] float movementSpeed;
@@ -10,5 +10,36 @@ public class EnemyStat : MonoBehaviour
 
     public float Health { get { return health; } }
     public float MovementSpeed { get { return movementSpeed; } }
-    public float Damage { get { return damage; } }
+
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth -= health;
+    }
+
+    public void Damage(DamageInfo damageInfo)
+    {
+        currentHealth -= damageInfo.damage;
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Throwable"))
+        {
+            Damage(new DamageInfo() { damage = 1 });
+        }
+        else if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            col.gameObject.GetComponent<IDamagable>().Damage(new DamageInfo() { damage = this.damage });
+        }
+    }
 }
--- UnityProject/Assets/Scripts/Player/PlayerController.cs
+++ UnityProject/Assets/Scripts/Player/PlayerController.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDamagable
 {
     [Header("Player attributes")]
+    public float health;
     public float movementSpeed = 4.0f;
     public float jumpHeight = 2.5f;
     public float rotateRate = 450.0f;
@@ -32,6 +33,7 @@ public class PlayerController : MonoBehaviour
     private int playerLayer;
 
     private Collider[] overlapResult = new Collider[10];
+    private float currentHealth;
 
     private void Awake()
     {
@@ -46,6 +48,8 @@ public class PlayerController : MonoBehaviour
         ChangeUpperBodyState(PlayerState.GetStateObject<UnequipedState>());
 
         marker.transform.SetParent(null);
+
+        currentHealth = health;
     }
 
     private void Update()
@@ -262,6 +266,19 @@ public class PlayerController : MonoBehaviour
         //{
         //    marker.SetActive(false);
         //}
+    }
+
+    public void Damage(DamageInfo damageInfo)
+    {
+        currentHealth -= damageInfo.damage;
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
 
