811bcc7e57e9f267ea542525c838f357b912d4b9
Tống Tùng Giang <tongtunggiang@gmail.com>
2019-11-28 10:13:17 +0000

    Enemy movement

---
+++ UnityProject/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : EnemyBehavior
+{
+    [Header("Movement stats")]
+    [SerializeField] float moveMagnitude;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        StartCoroutine(Oscillate());
+    }
+
+    private IEnumerator Oscillate()
+    {
+        while (true)
+        {
+            transform.position = new Vector3(Mathf.Cos(Time.time * Stat.MovementSpeed) * moveMagnitude, transform.position.y, 0) ;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
+
---
+++ UnityProject/Assets/Scripts/Enemies/EnemyStat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : MonoBehaviour
+{
+    [SerializeField] float health;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float damage;
+
+    public float Health { get { return health; } }
+    public float MovementSpeed { get { return movementSpeed; } }
+    public float Damage { get { return damage; } }
+}
